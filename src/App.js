8d27e2deae72c84b61c9d2324ed688cc204a7e18
Mustafa KANLI <mustafakanli98@gmail.com>
2022-05-14 03:52:39 +0300

    Added navigation bar, event page, community page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./Pages/Login/Login";
 import HomePage from "./Pages/Home/HomePage";
+import EventPage from "./Pages/Event/EventPage";
+import CommunityPage from "./Pages/Community/CommunityPage";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         {/*<Route path="/login" element={<Login />} />*/}
         <Route path="/" element={<HomePage />} />
+        <Route path="/event" element={<EventPage />} />
+        <Route path="/community" element={<CommunityPage />} />
       </Routes>
     </div>
   );
--- src/components/EventCard/Card.js
+++ src/components/EventCard/Card.js
@@ -1,37 +1,40 @@
 import React from "react";
-import "./card.css"
-import {TiLocation} from "react-icons/ti";
-import {BsFillCalendarCheckFill} from "react-icons/bs";
+import classes from "./Card.module.css";
+import { TiLocation } from "react-icons/ti";
+import { BsFillCalendarCheckFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Card = () => {
   return (
     <div>
-      
-      <div className="Card">
-        <div className="Card_image">
-        </div>
-       
-        <div className="Card_content">
-        <h1>Event Name</h1>
-        <p>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bil
-          inmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karış
-          tırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.</p>
-        <div className="Card_detail">
-        <div>
-          <BsFillCalendarCheckFill/>
-            01/06/2022 19.00
+      <Link to="/event" className={classes.link}>
+        <div className={classes.card}>
+          <div className={classes.cardImage}></div>
+
+          <div className={classes.cardContent}>
+            <h1 className={classes.cardHeader}>Event Name</h1>
+            <p>
+              Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
+              metinlerdir. Lorem Ipsum, adı bil inmeyen bir matbaacının bir
+              hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak
+              karış tırdığı 1500'lerden beri endüstri standardı sahte metinler
+              olarak kullanılmıştır.
+            </p>
+            <div className={classes.cardDetail}>
+              <div>
+                <BsFillCalendarCheckFill />
+                01/06/2022 19.00
+              </div>
+              <div>
+                <TiLocation />
+                Akdeniz Üniversitesi
+              </div>
+              <div>#Career#Summit</div>
+              <button className={classes.cardDetailButton}>Katıl</button>
+            </div>
           </div>
-          <div>
-            <TiLocation/>Akdeniz Üniversitesi
-          </div>
-          <div>
-            #Career#Summit
-          </div>
-          <button className="Card_detail-button">Katıl</button>
         </div>
-        </div>
-       
-      </div>
+      </Link>
     </div>
   );
 };
